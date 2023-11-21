3a733a7a43aaa78f7ef8aad985dcc80d87524e48
Meghana-Palaka <106471253+Meghana-Palaka@users.noreply.github.com>
2023-11-21 11:11:30 +0530

    Update Welcome.java

--- src/com/edubridge/app1/Welcome.java
+++ src/com/edubridge/app1/Welcome.java
@@ -2,7 +2,7 @@ package com.edubridge.app1;
 public class Welcome {
 	public static void main(String[] args) {
 		System.out.println("Welcome To Java");
-		System.out.println("Welcome To Java");
+		System.out.println("Welcome To Python");
 	}
 
 }
